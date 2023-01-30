9483a56a802cbc3f6c676fe9be2cf736c2ad2686
satyajit nayak <satyajitn15@gmail.com>
2023-01-30 21:37:44 +0530

    Add basic configuration fo DB schema in orders service.

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,21 @@
+import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
